b5b035ea748e622f758a4e2abe72c26dad64e3db
NiklasJang <niklasjang@gmailcom>
2019-04-09 20:01:57 +0900

    I did it! Generating map from gazebo!

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/niklasjang/catkin_ws/src/mybot_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/niklasjang/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/niklasjang/catkin_ws/devel;/opt/ros/kinetic" != "" else []
